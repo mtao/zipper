e3bb8bbd6a5b1d05d0c9448250ef4581de3ce5a8
Michael Tao <michael.tao@nyu.edu>
2025-03-12 18:14:04 -0400

    update

--- include/uvl/detail/dynamic_extent_indices.hpp
+++ include/uvl/detail/dynamic_extent_indices.hpp
@@ -1,19 +1,39 @@
 
+#if !defined(UVL_DETAIL_DYNAMIC_EXTENT_INDICES_HPP)
 #define UVL_DETAIL_DYNAMIC_EXTENT_INDICES_HPP
 
 #include "uvl/types.hpp"
 
 namespace uvl::detail {
 
-//template <index_type... Indices>
-//constexpr auto dynamic_swizzle_extents(index_type... Indices) {
-//    using E = extents<Indices...>;
-//}
-template <index_type... Indices>
-constexpr auto dynamic_swizzle_extents(const extents<Indices...>) {
+// template <index_type... Indices>
+// constexpr auto dynamic_extent_indices(index_type... Indices) {
+//     using E = extents<Indices...>;
+// }
+template <typename T>
+struct DynamicExtentIndices;
 
-    using E = extents<Indices...>;
-}
+template <index_type... indices>
+struct DynamicExtentIndices<extents<indices...>> {
+    constexpr static auto _eval() {
+        using E = extents<indices...>;
+        std::array<rank_type, E::rank_dynamic()> r;
+        rank_type idx = 0;
+        for (rank_type j = 0; j < E::rank(); ++j) {
+            if (E::static_extent(j) == std::dynamic_extent) {
+                r[idx++] = j;
+            }
+        }
+        return r;
+    }
+
+    constexpr static auto value = _eval();
+
+    //
+};
+
+template <typename T>
+constexpr static auto dynamic_extent_indices_t = DynamicExtentIndices<T>::value;
 
 }  // namespace uvl::detail
 #endif
