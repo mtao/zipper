fbb1773c6a0de1ee1be3f8a33f3282159a81dc11
Michael Tao <michael.tao@nyu.edu>
2025-07-12 11:40:31 -0400

    updating setup script, but will delete soon

--- setup.sh
+++ setup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-#takes as input a single arg that is the meson profile name. otherwise it uses the default one
-profile=${1}
-if [[ -n "$profile" ]]; then
-    conan_profile=$profile
-    build_folder=build-$profile
-else
-    conan_profile=default
-    build_folder=build
-fi
 
-mkdir -p $build_folder
-conan install . --output-folder=$build_folder/conan --build=missing --profile=$conan_profile
-pushd $build_folder
-meson setup --native-file conan/conan_meson_native.ini .. .
-ninja # build
+function run() {
+    #takes as input a single arg that is the meson profile name. otherwise it uses the default one
+    profile=${1}
+    if [[ -n "$profile" ]]; then
+        conan_profile=$profile
+        build_folder=build-$profile
+    else
+        conan_profile=default
+        build_folder=build
+    fi
+
+    mkdir -p $build_folder
+    conan install . --output-folder=$build_folder/conan --build=missing --profile=$conan_profile
+    pushd $build_folder
+    meson setup --native-file conan/conan_meson_native.ini .. .
+    ninja # build
+}
+
+
+for profile in $@; do
+    run "$profile"
+
+done
