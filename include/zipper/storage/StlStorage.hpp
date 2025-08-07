3255cda1c06ef901ffa75ae67018feb6b318a805
Michael Tao <michael.tao@nyu.edu>
2025-08-07 14:16:32 -0400

    stl based storage mechanism mostly works

--- include/zipper/storage/StlStorage.hpp
+++ include/zipper/storage/StlStorage.hpp
@@ -4,49 +4,32 @@
 #include <cstdint>
 #include <vector>
 
+#include "zipper/concepts/IndexPackLike.hpp"
+#include "zipper/detail/extents/dynamic_extents_indices.hpp"
 #include "zipper/types.hpp"
 
 namespace zipper::storage {
 
 namespace detail {
+// internal helper for managing storage info, order of its extents are indexed
+// in reverse to the final extents used because the left-most (0th extent) is
+// the outermost structure
 template <typename T>
-struct StlStorageInfo {};
-template <>
-struct StlStorageInfo<std::vector<int64_t>> {
-    constexpr static rank_type rank = 1;
-    constexpr static index_type static_extent = std::dynamic_extent;
-    static auto& get_value(std::vector<int64_t>& A, index_type i) {
-        return A[i];
-    }
-    static const auto& get_value(const std::vector<int64_t>& A, index_type i) {
-        return A[i];
-    }
-    // using extents_type = zipper::extents<std::dynamic_extent>();
-    // static extents_type extents(const std::vector<int64_t>& A) {
-    //     return extents_type(A.size());
-    // }
-};
-template <size_t N>
-struct StlStorageInfo<std::array<int64_t, N>> {
-    constexpr static rank_type rank = 1;
-    constexpr static index_type static_extent = index_type(N);
-    static const auto& get_value(const std::array<int64_t, N>& A,
-                                 index_type i) {
-        return A[i];
-    }
-    static auto& get_value(std::array<int64_t, N>& A, index_type i) {
-        return A[i];
-    }
+struct StlStorageInfo {
+    using value_type = T;
+    constexpr static rank_type rank = 0;
+    static auto& get_value(T& A) { return A; }
+    static const auto& get_value(const T& A) { return A; }
+    template <rank_type N>
+    using get_type = std::enable_if<(N == -1), value_type>;
 
-    // using extents_type = zipper::extents<N>();
-    // constexpr static extents_type extents(const std::array<int64_t, N>& A) {
-    //     return extents_type();
-    // }
+    consteval bool is_valid_extent(const T&) { return true; }
 };
 template <typename S>
 struct StlStorageInfo<std::vector<S>> : public StlStorageInfo<S> {
+    using self_type = std::vector<S>;
     constexpr static rank_type rank = StlStorageInfo<S>::rank + 1;
-    constexpr static index_type static_extent = std::dynamic_extent;
+    constexpr static index_type my_static_extent = std::dynamic_extent;
     template <typename... Args>
     static auto& get_value(std::vector<S>& A, index_type i, Args&&... args) {
         return StlStorageInfo<S>::get_value(A[i], std::forward<Args>(args)...);
@@ -56,11 +39,51 @@ struct StlStorageInfo<std::vector<S>> : public StlStorageInfo<S> {
                                  Args&&... args) {
         return StlStorageInfo<S>::get_value(A[i], std::forward<Args>(args)...);
     }
+
+    template <rank_type N>
+        requires(rank >= 0)
+    using get_type =
+        std::conditional_t<N + 1 == rank, self_type,
+                           typename StlStorageInfo<S>::template get_type<N>>;
+
+    template <rank_type N>
+    consteval static index_type static_extent() {
+        if constexpr (N + 1 == rank) {
+            return my_static_extent;
+        } else {
+            return StlStorageInfo<get_type<N>>::template static_extent<N>();
+        }
+    }
+    template <rank_type M>
+    static index_type extent(const self_type& s) {
+        if constexpr (M + 1 == rank) {
+            return s.size();
+        } else if constexpr (s.size() == 0) {
+            return 0;
+        } else {
+            return StlStorageInfo<S>::template extent<M>(s[0]);
+        }
+    }
+    template <typename... Args>
+    consteval bool is_valid_extent(const self_type& v, index_type size,
+                                   Args&&... args) {
+        if (v.size() != size) {
+            return false;
+        }
+        for (const auto& x : v) {
+            if (!StlStorageInfo<S>::is_valid_extent(
+                    x, std::forward<Args>(args)...)) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 template <size_t N, typename S>
 struct StlStorageInfo<std::array<S, N>> : public StlStorageInfo<S> {
+    using self_type = std::array<S, N>;
     constexpr static rank_type rank = StlStorageInfo<S>::rank + 1;
-    constexpr static index_type static_extent = index_type(N);
+    constexpr static index_type my_static_extent = index_type(N);
     template <typename... Args>
     static auto& get_value(std::array<S, N>& A, index_type i, Args&&... args) {
         return StlStorageInfo<S>::get_value(A[i], std::forward<Args>(args)...);
@@ -70,15 +93,143 @@ struct StlStorageInfo<std::array<S, N>> : public StlStorageInfo<S> {
                                  Args&&... args) {
         return StlStorageInfo<S>::get_value(A[i], std::forward<Args>(args)...);
     }
+    template <rank_type M>
+        requires(rank >= 0)
+    using get_type =
+        std::conditional_t<M + 1 == rank, self_type,
+                           typename StlStorageInfo<S>::template get_type<M>>;
+
+    template <rank_type M>
+    consteval static index_type static_extent() {
+        if constexpr (M + 1 == rank) {
+            return my_static_extent;
+        } else {
+            return StlStorageInfo<get_type<M>>::template static_extent<M>();
+        }
+    }
+    template <rank_type M>
+    constexpr static index_type extent(const self_type& s) {
+        if constexpr (M + 1 == rank) {
+            return my_static_extent;
+            ;
+        } else if constexpr (N == 0) {
+            return 0;
+        } else {
+            return StlStorageInfo<S>::template extent<M>(s[0]);
+        }
+    }
+    template <typename>
+    struct detail_;
+    template <rank_type... Ms>
+    struct detail_<std::integer_sequence<rank_type, Ms...>> {
+        //
+    };
+    using detail =
+        detail_<decltype(std::make_integer_sequence<rank_type, rank>{})>;
+
+    template <typename... Args>
+    consteval bool is_valid_extent(const self_type& v, index_type size,
+                                   Args&&... args) {
+        if (v.size() != size) {
+            return false;
+        }
+        for (const auto& x : v) {
+            if (!StlStorageInfo<S>::is_valid_extent(
+                    x, std::forward<Args>(args)...)) {
+                return false;
+            }
+        }
+        return true;
+    }
     // using extents_type = zipper::extents<N>();
     // constexpr static extents_type extents(const std::array<int64_t, N>& A) {
     //     return extents_type();
     // }
 };
-
-//using extents_type =
+// using extents_type =
 
 }  // namespace detail
+
+template <typename S>
+struct StlStorageInfo : public detail::StlStorageInfo<S> {
+    using internal_info = detail::StlStorageInfo<S>;
+
+    constexpr static rank_type rank = internal_info::rank;
+    template <typename>
+    struct detail_;
+    template <rank_type... Ms>
+    struct detail_<std::integer_sequence<rank_type, Ms...>> {
+        using extents_type =
+            extents<internal_info::template static_extent<rank - 1 - Ms>()...>;
+        //
+    };
+    using detail =
+        detail_<decltype(std::make_integer_sequence<rank_type, rank>{})>;
+    using extents_type = typename detail::extents_type;
+
+    template <rank_type... Ms>
+    static extents_type make_extents(const S& s,
+                                     std::integer_sequence<rank_type, Ms...>) {
+        constexpr auto dyn_indices =
+            zipper::detail::extents::dynamic_extents_indices_v<extents_type>;
+
+        return extents_type{std::array<index_type, dyn_indices.size()>{
+            {internal_info::template extent<rank - 1 - dyn_indices[Ms]>(
+                s)...}}};
+    }
+
+    static extents_type make_extents(const S& s) {
+        constexpr auto dyn_indices =
+            zipper::detail::extents::dynamic_extents_indices_v<extents_type>;
+        return make_extents(
+            s, std::make_integer_sequence<rank_type, dyn_indices.size()>{});
+    }
+
+    template <zipper::concepts::IndexLike... Args>
+    static auto& get_value(auto& v, Args&&... args) {
+        return internal_info::get_value(v, std::forward<Args>(args)...);
+    }
+    template <zipper::concepts::IndexLike... Args>
+    static const auto& get_value(const auto& v, Args&&... args) {
+        return internal_info::get_value(v, std::forward<Args>(args)...);
+    }
+};
+
+template <typename S>
+struct StlStorage {
+   public:
+    using info_helper = StlStorageInfo<S>;
+    using extents_type = typename info_helper::extents_type;
+    using value_type = typename info_helper::value_type;
+    consteval static index_type rank() { return info_helper::rank(); }
+    consteval static index_type static_extent(rank_type r) {
+        return extents_type::static_extent;
+    }
+    StlStorage(const S& d) : m_data(d) {}
+
+    extents_type extents() const { return info_helper::make_extents(m_data); }
+
+    template <zipper::concepts::IndexLike... Args>
+    const value_type& const_coeff_ref(Args&&... args) const {
+        return info_helper::get_value(m_data, std::forward<Args>(args)...);
+    }
+    template <zipper::concepts::IndexLike... Args>
+    value_type& coeff_ref(Args&&... args) {
+        return info_helper::get_value(m_data, std::forward<Args>(args)...);
+    }
+    template <zipper::concepts::IndexLike... Args>
+    value_type coeff(Args&&... args) const {
+        return info_helper::get_value(m_data, std::forward<Args>(args)...);
+    }
+
+   private:
+    S m_data;
+};
+
+template <typename S>
+StlStorage(const S&) -> StlStorage<S>;
+template <typename S>
+StlStorageInfo(const S&) -> StlStorageInfo<S>;
 
 }  // namespace zipper::storage
 
---
+++ tests/storage/stl_storage.cpp
@@ -0,0 +1,190 @@
+
+#include <zipper/storage/StlStorage.hpp>
+
+#include "../catch_include.hpp"
+
+TEST_CASE("stl_storage_basic_types", "[data]") {
+    {
+        using T = double;
+        using InfoType = zipper::storage::detail::StlStorageInfo<T>;
+        static_assert(InfoType::rank == 0);
+        static_assert(std::is_same_v<double, InfoType::value_type>);
+    }
+
+    {
+        using T = std::vector<double>;
+        using InfoType = zipper::storage::detail::StlStorageInfo<T>;
+        static_assert(InfoType::rank == 1);
+        static_assert(std::is_same_v<double, InfoType::value_type>);
+        static_assert(
+            std::is_same_v<T, typename InfoType::template get_type<0>>);
+        static_assert(InfoType::static_extent<0>() == zipper::dynamic_extent);
+
+        std::vector<double> x = {0, 1, 2};
+        zipper::storage::StlStorage storage(x);
+        auto e = storage.extents();
+        static_assert(e.static_extent(0) == std::dynamic_extent);
+        CHECK(e.extent(0) == 3);
+
+        CHECK(storage.coeff(0) == 0);
+        CHECK(storage.coeff(1) == 1);
+        CHECK(storage.coeff(2) == 2);
+        CHECK(storage.coeff_ref(0) == 0);
+        CHECK(storage.coeff_ref(1) == 1);
+        CHECK(storage.coeff_ref(2) == 2);
+        CHECK(storage.const_coeff_ref(0) == 0);
+        CHECK(storage.const_coeff_ref(1) == 1);
+        CHECK(storage.const_coeff_ref(2) == 2);
+
+        storage.coeff_ref(0) = 4;
+        storage.coeff_ref(1) = 5;
+        storage.coeff_ref(2) = 6;
+        CHECK(storage.const_coeff_ref(0) == 4);
+        CHECK(storage.const_coeff_ref(1) == 5);
+        CHECK(storage.const_coeff_ref(2) == 6);
+    }
+
+    {
+        using T = std::array<double, 5>;
+        using InfoType = zipper::storage::detail::StlStorageInfo<T>;
+        static_assert(InfoType::rank == 1);
+        static_assert(std::is_same_v<double, InfoType::value_type>);
+        static_assert(
+            std::is_same_v<T, typename InfoType::template get_type<0>>);
+        static_assert(InfoType::static_extent<0>() == 5);
+
+        std::array<double, 3> x{{0, 1, 2}};
+        zipper::storage::StlStorage storage(x);
+        auto e = storage.extents();
+        static_assert(e.static_extent(0) == 3);
+        CHECK(e.extent(0) == 3);
+
+        CHECK(storage.coeff(0) == 0);
+        CHECK(storage.coeff(1) == 1);
+        CHECK(storage.coeff(2) == 2);
+        CHECK(storage.coeff_ref(0) == 0);
+        CHECK(storage.coeff_ref(1) == 1);
+        CHECK(storage.coeff_ref(2) == 2);
+        CHECK(storage.const_coeff_ref(0) == 0);
+        CHECK(storage.const_coeff_ref(1) == 1);
+        CHECK(storage.const_coeff_ref(2) == 2);
+
+        storage.coeff_ref(0) = 4;
+        storage.coeff_ref(1) = 5;
+        storage.coeff_ref(2) = 6;
+        CHECK(storage.const_coeff_ref(0) == 4);
+        CHECK(storage.const_coeff_ref(1) == 5);
+        CHECK(storage.const_coeff_ref(2) == 6);
+    }
+    {
+        using T = std::vector<std::array<double, 5>>;
+        using InfoType = zipper::storage::detail::StlStorageInfo<T>;
+        static_assert(InfoType::rank == 2);
+        static_assert(std::is_same_v<double, InfoType::value_type>);
+
+        static_assert(std::is_same_v<std::array<double, 5>,
+                                     typename InfoType::template get_type<0>>);
+        static_assert(
+            std::is_same_v<T, typename InfoType::template get_type<1>>);
+        static_assert(InfoType::static_extent<0>() == 5);
+        static_assert(InfoType::static_extent<1>() == zipper::dynamic_extent);
+
+        std::vector<std::array<double, 3>> x{{0, 1, 2}, {3, 4, 5}};
+
+        zipper::storage::StlStorage storage(x);
+        auto e = storage.extents();
+        static_assert(decltype(e)::static_extent(0) == std::dynamic_extent);
+        static_assert(decltype(e)::static_extent(1) == 3);
+        CHECK(e.extent(0) == 2);
+        CHECK(e.extent(1) == 3);
+
+        CHECK(storage.coeff(0, 0) == 0);
+        CHECK(storage.coeff(0, 1) == 1);
+        CHECK(storage.coeff(0, 2) == 2);
+        CHECK(storage.coeff_ref(0, 0) == 0);
+        CHECK(storage.coeff_ref(0, 1) == 1);
+        CHECK(storage.coeff_ref(0, 2) == 2);
+        CHECK(storage.const_coeff_ref(0, 0) == 0);
+        CHECK(storage.const_coeff_ref(0, 1) == 1);
+        CHECK(storage.const_coeff_ref(0, 2) == 2);
+
+        CHECK(storage.coeff(1, 0) == 3);
+        CHECK(storage.coeff(1, 1) == 4);
+        CHECK(storage.coeff(1, 2) == 5);
+        CHECK(storage.coeff_ref(1, 0) == 3);
+        CHECK(storage.coeff_ref(1, 1) == 4);
+        CHECK(storage.coeff_ref(1, 2) == 5);
+        CHECK(storage.const_coeff_ref(1, 0) == 3);
+        CHECK(storage.const_coeff_ref(1, 1) == 4);
+        CHECK(storage.const_coeff_ref(1, 2) == 5);
+
+        storage.coeff_ref(0, 0) = 14;
+        storage.coeff_ref(0, 1) = 15;
+        storage.coeff_ref(0, 2) = 16;
+        storage.coeff_ref(1, 0) = 24;
+        storage.coeff_ref(1, 1) = 25;
+        storage.coeff_ref(1, 2) = 26;
+        CHECK(storage.const_coeff_ref(0, 0) == 14);
+        CHECK(storage.const_coeff_ref(0, 1) == 15);
+        CHECK(storage.const_coeff_ref(0, 2) == 16);
+        CHECK(storage.const_coeff_ref(1, 0) == 24);
+        CHECK(storage.const_coeff_ref(1, 1) == 25);
+        CHECK(storage.const_coeff_ref(1, 2) == 26);
+    }
+
+    {
+        using T = std::array<std::vector<double>, 5>;
+        using InfoType = zipper::storage::detail::StlStorageInfo<T>;
+        static_assert(InfoType::rank == 2);
+        static_assert(std::is_same_v<double, InfoType::value_type>);
+
+        static_assert(std::is_same_v<std::vector<double>,
+                                     typename InfoType::template get_type<0>>);
+        static_assert(
+            std::is_same_v<T, typename InfoType::template get_type<1>>);
+        static_assert(InfoType::static_extent<0>() == zipper::dynamic_extent);
+        static_assert(InfoType::static_extent<1>() == 5);
+
+        std::array<std::vector<double>, 2> x{{{0, 1, 2}, {3, 4, 5}}};
+
+        zipper::storage::StlStorage storage(x);
+        auto e = storage.extents();
+        static_assert(decltype(e)::static_extent(0) == 2);
+        static_assert(decltype(e)::static_extent(1) == std::dynamic_extent);
+        CHECK(e.extent(0) == 2);
+        CHECK(e.extent(1) == 3);
+
+        CHECK(storage.coeff(0, 0) == 0);
+        CHECK(storage.coeff(0, 1) == 1);
+        CHECK(storage.coeff(0, 2) == 2);
+        CHECK(storage.coeff_ref(0, 0) == 0);
+        CHECK(storage.coeff_ref(0, 1) == 1);
+        CHECK(storage.coeff_ref(0, 2) == 2);
+        CHECK(storage.const_coeff_ref(0, 0) == 0);
+        CHECK(storage.const_coeff_ref(0, 1) == 1);
+        CHECK(storage.const_coeff_ref(0, 2) == 2);
+
+        CHECK(storage.coeff(1, 0) == 3);
+        CHECK(storage.coeff(1, 1) == 4);
+        CHECK(storage.coeff(1, 2) == 5);
+        CHECK(storage.coeff_ref(1, 0) == 3);
+        CHECK(storage.coeff_ref(1, 1) == 4);
+        CHECK(storage.coeff_ref(1, 2) == 5);
+        CHECK(storage.const_coeff_ref(1, 0) == 3);
+        CHECK(storage.const_coeff_ref(1, 1) == 4);
+        CHECK(storage.const_coeff_ref(1, 2) == 5);
+
+        storage.coeff_ref(0, 0) = 14;
+        storage.coeff_ref(0, 1) = 15;
+        storage.coeff_ref(0, 2) = 16;
+        storage.coeff_ref(1, 0) = 24;
+        storage.coeff_ref(1, 1) = 25;
+        storage.coeff_ref(1, 2) = 26;
+        CHECK(storage.const_coeff_ref(0, 0) == 14);
+        CHECK(storage.const_coeff_ref(0, 1) == 15);
+        CHECK(storage.const_coeff_ref(0, 2) == 16);
+        CHECK(storage.const_coeff_ref(1, 0) == 24);
+        CHECK(storage.const_coeff_ref(1, 1) == 25);
+        CHECK(storage.const_coeff_ref(1, 2) == 26);
+    }
+}
