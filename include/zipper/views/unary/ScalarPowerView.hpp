3126178ab4c6cedc30278df619e592907280da9c
Michael Tao <michael.tao@nyu.edu>
2025-08-28 15:51:16 -0400

    osx clang is pedantic about pow<T> be a value, so decaying it

--- include/zipper/views/unary/ScalarPowerView.hpp
+++ include/zipper/views/unary/ScalarPowerView.hpp
@@ -11,7 +11,7 @@ namespace detail {
 template <typename T>
 struct pow {
     constexpr auto operator()(const T& a, const T& b) const {
-        return std::pow<std::decay_t<T>>(a, b);
+        return std::pow<std::decay_t<T>>(T(a), T(b));
     }
 };
 }  // namespace detail
